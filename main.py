c92b05062bdc776e61220b4074105a7242158512
Lawrence <lawrence.archer@gmail.com>
2020-01-18 00:33:51 +0000

    change method for counting time to time.time()

--- main.py
+++ main.py
@@ -3,23 +3,24 @@ import datetime, time
 project_data =	{
   "programming": 0
 }
-
+print(time.clock())
+print(time.time())
 def start_tracker():
     name = input("What project are you working on?\n")
-    start=datetime.datetime.now()
+    start=time.time()
     timer_active = True
 
     user_input = ""
     while user_input != "stop":
         user_input = input("Let me know when you want to stop")
-    end=datetime.datetime.now()
-    time = end-start
-    print(time)
+    end=time.time()
+    elapsed_time = end-start
+    print(elapsed_time)
     
     if name in project_data:
-        project_data[name] += time
+        project_data[name] += elapsed_time
     else:
-        project_data.update({name: time})
+        project_data.update({name: elapsed_time})
     print(project_data)
 
 
