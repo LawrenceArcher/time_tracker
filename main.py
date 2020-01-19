10c026f9478116536210c02f9c6aaf27cecc8703
Lawrence <lawrence.archer@gmail.com>
2020-01-19 14:25:09 +0000

    add ability to save to json

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import datetime, time
+import datetime, time, json
 
-project_data =	{
-  "programming": 0
-}
-print(time.clock())
-print(time.time())
+with open('project_data.json', 'r') as project_data:
+    data=project_data.read()
+        
+project_data = json.loads(data)
+
 def start_tracker():
     name = input("What project are you working on?\n")
     start=time.time()
@@ -14,16 +14,17 @@ def start_tracker():
     while user_input != "stop":
         user_input = input("Let me know when you want to stop")
     end=time.time()
-    elapsed_time = end-start
+    elapsed_time = int(end-start)
     print(elapsed_time)
     
     if name in project_data:
         project_data[name] += elapsed_time
     else:
         project_data.update({name: elapsed_time})
+
     print(project_data)
-
-
+    with open('project_data.json', 'w') as data_to_write:
+        json.dump(project_data, data_to_write)
 
 
 start_tracker()
--- test.py
+++ test.py
@@ -1,24 +1,36 @@
-project_data =	{"programming": 1}
+import json, datetime
 
-def add_new_entry():
-    time = 5
-    name = str(input("project?\n"))
+# with open('project_info.json') as json_file:
+#     data = json.load(json_file)
+#     for p in data:
+#         print(p)
+time_elapsed1 = datetime.timedelta(seconds=666)
+time_elapsed2 = datetime.timedelta(seconds=555)
+
+
+print(time_elapsed1)
+print(time_elapsed2)
+print(time_elapsed1+time_elapsed2)
+
+# def add_new_entry():
+#     time = 5
+#     name = str(input("project?\n"))
     
-    increment = {name: time}
-    print(increment)
-    print({name, time})
-    if name in project_data:
-        project_data[name] += time
-    else:
-        project_data.update(increment)
+#     increment = {name: time}
+#     print(increment)
+#     print({name, time})
+#     if name in project_data:
+#         project_data[name] += time
+#     else:
+#         project_data.update(increment)
 
 
-add_new_entry()
+# add_new_entry()
 
-# new_data = {"music": 5}
-# project_data.update(new_data)
+# # new_data = {"music": 5}
+# # project_data.update(new_data)
 
-print(project_data)
+# print(project_data)
 
 # d = {1: "one", 2: "three"}
 # d1 = {2: "two"}
